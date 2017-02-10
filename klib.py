1854b273208195def86d3e366cd026f475444f5a
Kishore P V <kishorepv@users.noreply.github.com>
2017-02-10 14:38:59 +0530

    +Creation of validation data

--- klib.py
+++ klib.py
@@ -2,12 +2,13 @@ import glob
 import os
 import shutil
 import random
-def make_sample(sample_dir, train_dir, test_dir, size=10, sep='/'):
+
+def make_sample(sample_dir, train_dir, valid_dir, test_dir, size=10, sep='/'):
     if not os.path.isdir(sample_dir):
         os.mkdir(sample_dir)
-    train_name=train_dir.split(sep)[-1]
-    sample_train=os.path.join(sample_dir,train_name)
-    os.mkdir(sample_train)
+    #train_name=train_dir.split(sep)[-1]
+    #sample_train=os.path.join(sample_dir,train_name)
+    #os.mkdir(sample_train)
     test_name=test_dir.split(sep)[-1]
     sample_test=os.path.join(sample_dir,test_name)
     os.mkdir(sample_test)
@@ -21,10 +22,32 @@ def make_sample(sample_dir, train_dir, test_dir, size=10, sep='/'):
     files=random.sample(glob.glob(os.path.join(te_dir,'*')), size)
     for onefile in files:
         shutil.copy2(onefile, os.path.join(sample_test,te_dname))
-    for dir_ in glob.glob(os.path.join(train_dir,'*')):
+    for dirx in [train_dir, valid_dir]:
+        create_train_valid_sample(dirx, sample_dir, size, sep)
+
+def create_train_valid_sample(src_dir, dest_dir, size, sep):
+    dest_name=src_dir.split(sep)[-1]
+    dest_base=os.path.join(dest_dir,dest_name)
+    os.mkdir(dest_base)
+    for dir_ in glob.glob(os.path.join(src_dir,'*')):
         if (os.path.isdir(dir_)):
             dname=dir_.split(sep)[-1]
-            os.mkdir(os.path.join(sample_train,dname))
-            files=random.sample(glob.glob(os.path.join(dir_,'*')), size)
+            dest_subdir=os.path.join(dest_base,dname)
+            os.mkdir(dest_subdir)
+            all_files=glob.glob(os.path.join(dir_,'*'))
+            files=random.sample(all_files, size)
             for onefile in files:
-                shutil.copy2(onefile, os.path.join(sample_train,dname))
+                shutil.copy2(onefile, dest_subdir)
+
+def create_validation_data(train_dir, percent=0.1, sep='/'):
+    valid_dir=os.path.join(sep.join(train_dir.split(sep)[:-1]), "valid")
+    os.mkdir(valid_dir)
+    for dir_ in glob.glob(os.path.join(train_dir,"*")):
+        if os.path.isdir(dir_):
+            dname=dir_.split(sep)[-1]
+            valid_subdir=os.path.join(valid_dir,dname)
+            os.mkdir(valid_subdir)
+            all_files=glob.glob(os.path.join(dir_,'*'))
+            files=random.sample(all_files, int(len(all_files)*percent))
+            for onefile in files:
+                shutil.move(onefile, valid_subdir)
